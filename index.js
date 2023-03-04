78bdf53178438bc5418966953b17c7ad019c0457
Breno <breno.am2004@gmail.com>
2023-03-04 06:49:24 -0300

    feat: parcial front-end

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const body_parser = require("body-parser");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(body_parser.urlencoded({extended: false}));
+app.use(body_parser.json());
+
+app.get("/", (req, res) => {
+    res.render("hub/index.ejs");
+});
+
+app.listen(8080, () => {
+    console.log("Server open");
+});
